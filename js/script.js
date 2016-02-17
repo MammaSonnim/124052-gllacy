d7235c87a8ff0df5d6dd0182f9bf8beea7e77744
MammaSonnim <mammasonnim@users.noreply.github.com>
2016-02-17 20:40:46 +0300

    iss05 shadows

--- js/script.js
+++ js/script.js
@@ -5,17 +5,20 @@
 
   function init() {
     myMap = new ymaps.Map('map', {
-        center: [59.938554257894, 30.322479499999993],
+        center: [59.938429885190054, 30.32991749740603],
         zoom: [16],
         controls: []
       }),
       myMap.behaviors.disable('scrollZoom');
     myMap.controls.add('zoomControl');
 
-    myPlacemark = new ymaps.Placemark([59.938554257894, 30.322479499999993], {
+    myPlacemark = new ymaps.Placemark([59.93866675783276,30.32307250000002], {
       hintContent: 'г. Санкт-Петербург, ул. Б. Конюшенная, д. 19/8',
     }, {
-      preset: 'islands#redDotIcon'
+      iconLayout: 'default#image',
+      iconImageHref: './img/img_map-pin.png',
+      iconImageSize: [218, 142],
+      iconImageOffset: [-38, -125]
     });
 
     myMap.geoObjects.add(myPlacemark);
